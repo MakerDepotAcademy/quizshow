f1ab3ab5b6561a57af384eb8459cf08df81f13fe
Dan Habot <dan.habot@gmail.com>
2019-08-06 16:17:38 -0400

    fix /sigh

--- QuizShowGamePlay.py
+++ QuizShowGamePlay.py
@@ -3,11 +3,75 @@ from sqlalchemy import create_engine
 import time
 from threading import Thread, Event
 from display import Display
+from gpio32.lib import Manager
 
 # Event object used to send signals from one thread to another
 stopGameEvent = Event()
 
-D = Display('localhost:3000')
+D = Display('localhost:8080')
+Boards = Manager()
+
+class Button():
+
+    def __init__(self, board, pin_in, pin_out):
+        self._board = board
+        self._in = pin_in
+        self._out = pin_out
+
+    def light(self, on=True):
+        if on:
+            self._board.turnOn(self._out)
+        else:
+            self._board.turnOff(self._out)
+
+    def hook(self, h):
+        self._board.onChange(h, self._in)
+
+    def read(self):
+        return self._board.readPin(self._in)
+
+class Player():
+
+    def __init__(self, board, bot, top):
+        if (top - bot) / 2 != 4:
+            raise Exception('Range must be 8 pins long')
+
+        self.buttons = { l: Button(board, i, i + 1) for l, i in zip( ['a', 'b', 'c', 'd'], range(bot, top, 2) ) }
+        self._board = board
+
+    def lightAll(self, on=True):
+        for b in self.buttons:
+            self.buttons[b].light(on)
+
+    def __getitem__(self, x):
+        return self.buttons[x]
+
+    def catchAnswer(self):
+        ans = ''
+        def a(pin, val):
+            ans = 'a'
+        def b(pin, val):
+            ans = 'b'
+        def c(pin, val):
+            ans = 'c'
+        def d(pin, val):
+            ans = 'd'
+
+        self.buttons['a'].hook(a)
+        self.buttons['b'].hook(b)
+        self.buttons['c'].hook(c)
+        self.buttons['d'].hook(d)
+
+        self._board._eventThread.join()
+        return ans
+
+Players = {
+    'a': Player(Boards['65535'], 0, 8),
+    'b': Player(Boards['65535'], 9, 18),
+    'c': Player(Boards['65535'], 18, 27) # ,
+    # 'd': Player(Boards['1'], 0, 8),
+    # 'e': Player(Boards['1'], 9, 18)
+}
 
 # Ask Questions
 def AskQuestions():
@@ -15,6 +79,17 @@ def AskQuestions():
     dbConnect = create_engine('sqlite:///quizShow.db')
 
     dbConnection = dbConnect.connect()
+
+    def getPlayer():
+        i = 0
+        while True:
+            i += 1
+            if i > len(Players.keys()):
+                i = 0
+            yield Players[Players.keys()[i]]
+            
+    GP = getPlayer()
+             
     while (1):
         # Ask question and verify answer
         query = dbConnection.execute('''SELECT
@@ -35,6 +110,8 @@ def AskQuestions():
         #result = {'trivia_question': [dict(zip(tuple(query.keys()), i))
         #                              for i in query.cursor]}    
         for row in query:
+            thisPlayer = next(GP)
+
             print("rowid: ", row['rowid'])
             print("question: ", row['question'])
             print("yellow: ", row['yellow'])
@@ -43,20 +120,26 @@ def AskQuestions():
             print("blue: ", row['blue'])
             print("correct_answer: ", row['correct_answer'])
 
+            thisPlayer.lightAll()
+            time.sleep(3)
+            thisPlayer.lightAll(False)
+
+            D.setQuestion(row['question'])
             D.setAnswer('a', row['red'])
             D.setAnswer('b', row['green'])
             D.setAnswer('c', row['yellow'])
             D.setAnswer('d', row['blue'])
-            D.setQuestion(row['question'])
+            
             D.start()
 
             # Send question to the board and wait for answer
-            ans = input(row['question'])
+            ans = thisPlayer.catchAnswer()
             if ans != row['correct_answer']:
                 print("Wrong")
                 score = score - 1
             else:
                 print("Correct Answer")
+                D.setCorrect(ans)
                 score = score + 1
             D.setScore(score)
 
