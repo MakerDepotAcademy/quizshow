273c4be8d50d680e86ecd9ebcce8d45757b55f7b
Dan Habot <dan.habot@gmail.com>
2019-08-22 14:11:50 -0400

    Modified lib to operate on a queue system

--- gpio32/test_lib.py
+++ gpio32/test_lib.py
@@ -1,6 +1,24 @@
 import unittest
 import lib
 
+def forAllBoards(fn):
+  def wrapper(self):
+    for id in self.manager:
+      self.thisBoard = self.manager[id]
+      fn(self)
+
+  return wrapper
+
+def forAllPins(fn):
+  def wrapper(self):
+    for i in range(self.thisBoard._queuelen):
+      self.thisPin = i
+      fn(self)
+    self.thisBoard.run()
+    self.assertTrue(True)
+
+  return wrapper
+
 class Lib_Test(unittest.TestCase):
 
   @classmethod
@@ -11,6 +29,7 @@ class Lib_Test(unittest.TestCase):
   def tearDownClass(cls):
     cls.manager.closeall()
 
+  @forAllBoards
   def test_tryAllPins(self):
     for id in self.manager:
       m = self.manager[id]
@@ -21,6 +40,26 @@ class Lib_Test(unittest.TestCase):
         m.setInput(i, False)
 
     self.assertTrue(True)
+
+  @forAllBoards
+  @forAllPins
+  def test_turnAllOn(self):
+    self.thisBoard.turnOn(self.thisPin)
+
+  @forAllBoards
+  @forAllPins
+  def test_turnAllOff(self):
+    self.thisBoard.turnOff(self.thisPin)
+
+  @forAllBoards
+  @forAllPins
+  def test_setAllInputs(self):
+    self.thisBoard.setInput(self.thisPin)
+
+  @forAllBoards
+  @forAllPins
+  def test_setAllInputsInverse(self):
+    self.thisBoard.setInput(self.thisPin, True)
 
   def test_getLocation(self):
     for id in self.manager:
