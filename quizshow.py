a99622d3ced62091f56048c562a2eed6a4d156ce
Dan Habot <dan.habot@gmail.com>
2019-08-30 15:43:18 -0400

    d

--- quizshow.py
+++ quizshow.py
@@ -1,7 +1,7 @@
 import ptvsd
 
 # Allow other computers to attach to ptvsd at this IP address and port.
-ptvsd.enable_attach(address=('1.2.3.4', 3000), redirect_output=True)
+ptvsd.enable_attach(address=('0.0.0.0', 5678), redirect_output=True)
 
 # Pause the program until a remote debugger is attached
 ptvsd.wait_for_attach()
