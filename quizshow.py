fb1978a473e61832bb11ee2c310968783c06de79
Dan Habot <dan.habot@gmail.com>
2019-08-30 15:41:21 -0400

    debug

--- quizshow.py
+++ quizshow.py
@@ -1,3 +1,11 @@
+import ptvsd
+
+# Allow other computers to attach to ptvsd at this IP address and port.
+ptvsd.enable_attach(address=('1.2.3.4', 3000), redirect_output=True)
+
+# Pause the program until a remote debugger is attached
+ptvsd.wait_for_attach()
+
 import os, time
 
 from flask import Flask, request
